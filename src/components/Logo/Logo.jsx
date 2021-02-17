51a076a91fe44ace88b992f9877730800d523db7
Manulovich <manulovicm@gmail.com>
2021-02-17 22:06:00 +0300

    add css module in logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,12 +1,12 @@
-import './Logo.css';
+import LogoStyle from './Logo.module.css';
 
 
 const Logo = () => {
     return (
-        <div className="logo header__logo">
-            <h2 className="logo__part logo__part--left">its</h2>
+         <div className={`${LogoStyle.logo} header__logo`}>
+            <h2 className={`${LogoStyle.logo__part} ${LogoStyle['logo__part--left']}`}>its</h2>
             my
-            <h2 className="logo__part logo__part--right">profile</h2>
+            <h2 className={`${LogoStyle.logo__part} ${LogoStyle['logo__part--right']}`}>profile</h2>
         </div>
     );
 };
